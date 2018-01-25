7aa14a23b79297c01e7f97d68331f0f5cccb7d3c
Tan-Wenhao <paul.tanwh@gmail.com>
2018-01-25 12:59:26 +0800

    Add models ShippingCarrier & ShippingMethod

---
+++ common/models/ShippingCarrier.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017-01-20
+ * Time: 16:12
+ */
+
+namespace common\models;
+
+
+use yii\db\ActiveRecord;
+
+class ShippingCarrier extends ActiveRecord
+{
+
+}
